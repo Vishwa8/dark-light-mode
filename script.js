fc2f405aeccc80d7e1bbd990ba45273447f8a947
Vishwa <vishwadharsan8@gmail.com>
2021-03-30 16:59:50 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+const checkCurrentTheme = localStorage.getItem('theme');
+if (checkCurrentTheme) {
+    document.documentElement.setAttribute('data-theme', checkCurrentTheme);
+    if (checkCurrentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchThemeDynamically);
+
+function switchThemeDynamically(event) {
+    console.log(event.target.checked);
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');        
+        lightMode();
+    }
+}
+
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
+}
+
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+}
+
+function imageMode(color) {
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
